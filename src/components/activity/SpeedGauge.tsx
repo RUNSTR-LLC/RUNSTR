277b183bfa032b42140b41d95ef2576493d5be5b
Larry <larry@Larrys-Mac-mini.local>
2026-03-10 18:01:37 -0400

    cleanup: remove unused Circle import in SpeedGauge

--- src/components/activity/SpeedGauge.tsx
+++ src/components/activity/SpeedGauge.tsx
@@ -5,7 +5,7 @@
 
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import Svg, { Circle, Path, Text as SvgText } from 'react-native-svg';
+import Svg, { Path, Text as SvgText } from 'react-native-svg';
 import { theme } from '../../styles/theme';
 
 interface SpeedGaugeProps {
